d507c3a1d4df4cc445b1fa844e625b6cb78dc378
Varn1ka <157673718+Varn1ka@users.noreply.github.com>
2026-07-16 09:39:40 +0530

    Update ClosestSum.java

--- Day8/ClosestSum.java
+++ Day8/ClosestSum.java
@@ -1,1 +1,42 @@
-dn
+import java.util.Arrays;
+
+public class Solution {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return target; 
+                }
+            }
+        }
+
+        return closest;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+
+        System.out.println(sol.threeSumClosest(nums, target)); 
+    }
+}
