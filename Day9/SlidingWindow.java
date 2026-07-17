0dc016a4e758780f9c5699e1f35ab9077f114b37
Varn1ka <157673718+Varn1ka@users.noreply.github.com>
2026-07-17 13:41:56 +0530

    Update SlidingWindow.java

--- Day9/SlidingWindow.java
+++ Day9/SlidingWindow.java
@@ -1,1 +1,24 @@
-d
+public List<Integer> solve(int N, int[] arr, int target) {
+    List<Integer> ans = new ArrayList<>();
+
+    int start = 0;
+    int sum = 0;
+
+    for (int end = 0; end < N; end++) {
+        sum += arr[end];
+
+        while (sum > target && start <= end) {
+            sum -= arr[start];
+            start++;
+        }
+
+        if (sum == target) {
+            ans.add(start + 1); 
+            ans.add(end + 1);  
+            return ans;
+        }
+    }
+
+    ans.add(-1);
+    return ans;
+}
