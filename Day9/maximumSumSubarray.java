c5a230aed2a249a88b9c3064abe5f0331eb58e6c
Varn1ka <157673718+Varn1ka@users.noreply.github.com>
2026-07-17 13:42:28 +0530

    Update maximumSumSubarray.java

--- Day9/maximumSumSubarray.java
+++ Day9/maximumSumSubarray.java
@@ -1,2 +1,21 @@
-sO
+public static long maximumSumSubarray(long[] nums, long k) {
+    int n = nums.length;
 
+    if (k > n) return 0;
+
+    long windowSum = 0;
+
+    for (int i = 0; i < k; i++) {
+        windowSum += nums[i];
+    }
+
+    long maxSum = windowSum;
+
+    for (int i = (int) k; i < n; i++) {
+        windowSum += nums[i];
+        windowSum -= nums[i - (int) k];
+        maxSum = Math.max(maxSum, windowSum);
+    }
+
+    return maxSum;
+}
